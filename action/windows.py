baa4e928ff976978d1594ef8939ed0a119db2f61
滕汉川 <tenghc@seeyon.com>
2025-04-10 18:55:30 +0800

    init

--- action/windows.py
+++ action/windows.py
@@ -112,7 +112,7 @@ class WindowsInstallTools(InstallTools):
     def get_verify_code(self):
         verify_code = self.load_verify_code()
         if verify_code:
-            return verify_code
+            self.verify_code = verify_code
         self.run_verify_code()
         time.sleep(5)
         window = self.get_verify_code_window()
@@ -125,7 +125,6 @@ class WindowsInstallTools(InstallTools):
             self.verify_code = matches[0]
             print(f"验证码：{self.verify_code}")
             self.write_verify_code()
-            return self.verify_code
         raise Exception(f"AI获取验证码失败")
 
 
